4da07399bd967e554c7333f856a9b3c222ee42ae
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-11-24 20:29:43 +0000

    change config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +11,7 @@ const config = {
     })
   ],
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   }
