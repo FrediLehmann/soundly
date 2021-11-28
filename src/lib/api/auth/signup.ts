bf3c3469ee7859e25f024c1cf1fd2391d5733c48
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-11-28 10:18:02 +0000

    add supabase

--- src/lib/api/auth/signup.ts
+++ src/lib/api/auth/signup.ts
@@ -1,18 +1,10 @@
+import { supabase } from '$lib/db';
+import type { ApiError, Session, User } from '@supabase/supabase-js';
+
 const signup = async (
   email: string,
   pwd: string
-): Promise<{ email: string }> => {
-  return await new Promise((resolve, reject) => resolve({ email }));
-
-  // const { user } = await Auth.signUp({
-  //     username: email,
-  //     password: pwd,
-  //     attributes: {
-  //         email
-  //     }
-  // });
-
-  // return user
-};
+): Promise<{ user: User; session: Session; error: ApiError }> =>
+  supabase.auth.signUp({ email, password: pwd });
 
 export default signup;
