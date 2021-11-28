79c161a09e9c62453058b3da366a31fdba841972
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-11-28 10:21:54 +0000

    add button hover effect

--- src/lib/components/atoms/Button/ButtonTypes.enum.ts
+++ src/lib/components/atoms/Button/ButtonTypes.enum.ts
@@ -1,5 +1,5 @@
 export enum ButtonTypes {
-  Primary = 'my-auto px-5 py-2 h-full bg-blue-600 text-white',
-  Secondary = 'my-auto px-5 py-2 h-full bg-gray-300',
-  Ghost = ''
+    Primary = 'my-auto px-5 py-2 h-full bg-blue-600 text-white hover:bg-blue-800',
+    Secondary = 'my-auto px-5 py-2 h-full bg-gray-300 hover:bg-gray-400',
+    Ghost = ''
 }
