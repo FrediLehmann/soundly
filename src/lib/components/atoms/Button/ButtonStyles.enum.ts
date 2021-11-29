9c8043465b8fde59384234ceb45d02c8e9ca3375
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-11-29 20:55:14 +0000

    add ghost style

--- src/lib/components/atoms/Button/ButtonStyles.enum.ts
+++ src/lib/components/atoms/Button/ButtonStyles.enum.ts
@@ -1,5 +1,5 @@
 export enum ButtonStyles {
   Primary = 'my-auto px-5 py-2 h-full bg-blue-600 text-white hover:bg-blue-800',
   Secondary = 'my-auto px-5 py-2 h-full bg-gray-300 hover:bg-gray-400',
-  Ghost = ''
+  Ghost = 'px-2 rounded-sm border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white'
 }
