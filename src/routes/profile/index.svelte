6b7cd52711d5d4d5346b85b42cfff3a6e95b0127
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-12-02 12:36:25 +0000

    fix issues

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -4,6 +4,6 @@
 
 <BackLink href="/" class="mb-2 pt-4">Home</BackLink>
 <section class="grid-cols-2">
-  <label class="col-span-1">Email</label>
-  <input class="col-span-1" />
+  <label class="col-span-1" for="email">Email</label>
+  <input class="col-span-1" id="email" />
 </section>
