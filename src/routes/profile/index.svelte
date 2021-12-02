f1918f5239cd6211d0eeb2d7d69c63244f9b5674
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-12-02 10:08:04 +0000

    state

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -3,3 +3,7 @@
 </script>
 
 <BackLink href="/" class="mb-2 pt-4">Home</BackLink>
+<section class="grid-cols-2">
+  <label class="col-span-1">Email</label>
+  <input class="col-span-1" />
+</section>
