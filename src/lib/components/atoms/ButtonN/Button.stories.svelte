2eccdd13d89342a3226381c42bf2e75a4d965a32
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-12-08 20:36:31 +0000

    create first component & story

---
+++ src/lib/components/atoms/ButtonN/Button.stories.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+  import Button from './Button.svelte';
+</script>
+
+<Meta
+  title="Atoms/Button"
+  component={Button}
+  argTypes={{
+    type: {
+      control: { type: 'select' },
+      options: ['button', 'submit', 'reset']
+    },
+    style: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'ghost', 'link']
+    }
+  }} />
+
+<Template let:args>
+  <Button {...args} type={args.type}>Button</Button>
+  <Button {...args} type={args.type} disabled>Button</Button>
+</Template>
+
+<Story name="Primary" args={{ style: 'primary' }} />
+
+<Story name="Secondary" args={{ style: 'secondary' }} />
+
+<Story name="Ghost" args={{ style: 'ghost' }} />
+
+<Story name="Link" args={{ style: 'link' }} />
---
+++ src/lib/components/atoms/ButtonN/Button.svelte
@@ -0,0 +1,52 @@
+<script type="ts">
+  export let type: 'button' | 'submit' | 'reset' = 'button';
+  export let style: 'primary' | 'secondary' | 'ghost' | 'link' = 'secondary';
+</script>
+
+<button on:click {type} data-type={style} {...$$restProps}>
+  <slot />
+</button>
+
+<style>
+  :root {
+    --primary-color: hsl(204, 81%, 55%);
+    --primary-shadow: hsla(204, 81%, 55%, 50%);
+    --white: hsl(0, 0%, 100%);
+  }
+
+  button {
+    cursor: pointer;
+
+    padding-block: 0.5rem;
+    padding-inline: 1.25rem;
+
+    border: none;
+    border-radius: 0.125rem;
+
+    font-weight: 600;
+  }
+
+  button:disabled {
+    cursor: default;
+  }
+
+  button[data-type='primary'] {
+    color: var(--white);
+    background-color: var(--primary-color);
+  }
+
+  button[data-type='primary']:hover {
+    box-shadow: 0 0 0 2px var(--primary-shadow);
+  }
+
+  button[data-type='primary']:focus,
+  button[data-type='primary']:active {
+    box-shadow: 0 0 0 1px var(--white), 0 0 0 3px var(--primary-shadow),
+      0 0 #0000;
+  }
+
+  button[data-type='primary']:disabled {
+    box-shadow: none;
+    opacity: 0.7;
+  }
+</style>
