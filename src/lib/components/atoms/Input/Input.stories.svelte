7400827bcbd7ae4c97ba335714805fbbf0208f8d
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-12-14 18:53:40 +0000

    refactor input

---
+++ src/lib/components/atoms/Input/Input.stories.svelte
@@ -0,0 +1,105 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import { userEvent, within } from '@storybook/testing-library';
+
+  import Input from './Input.svelte';
+</script>
+
+<Meta
+  title="Atoms/Input"
+  component={Input}
+  argTypes={{
+    name: {
+      control: { type: 'text' },
+      table: {
+        type: {
+          summary: 'Input identification'
+        }
+      }
+    },
+    label: {
+      control: { type: 'text' },
+      table: {
+        type: {
+          summary: 'Input label text'
+        }
+      }
+    },
+    value: {
+      control: { type: 'text' },
+      table: {
+        type: {
+          summary: 'The value of the input, which can be bound'
+        }
+      }
+    },
+    error: {
+      control: { type: 'text' },
+      table: {
+        type: {
+          summary: 'Error text, in case there is an validation error'
+        }
+      }
+    },
+    required: {
+      control: { type: 'boolean' },
+      table: {
+        type: {
+          summary: 'Sets the input as required (showing the * symbol)'
+        }
+      }
+    },
+    labelSrOnly: {
+      control: { type: 'boolean' },
+      table: {
+        type: {
+          summary:
+            'Show or hide the label from the UI without hidding it from screen readers.'
+        }
+      }
+    }
+  }} />
+
+<Template let:args>
+  <Input {...args} />
+</Template>
+
+<Story
+  name="With label"
+  args={{ name: 'Test', label: 'This Input' }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole('textbox');
+    userEvent.type(input, 'Inputting a text, here.');
+  }} />
+
+<Story
+  name="With error"
+  args={{ name: 'Test', label: 'This Input', error: 'There is an error' }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole('textbox');
+    userEvent.type(input, 'Inputting a text, here.');
+  }} />
+
+<Story
+  name="Required"
+  args={{ name: 'Test', label: 'This Input', required: true }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole('textbox');
+    userEvent.type(input, 'Inputting a text, here.');
+  }} />
+
+<Story
+  name="Hidden label"
+  args={{ name: 'Test', label: 'This Input', labelSrOnly: true }}
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole('textbox');
+    userEvent.type(input, 'Inputting a text, here.');
+  }} />
--- src/lib/components/atoms/Input/Input.svelte
+++ src/lib/components/atoms/Input/Input.svelte
@@ -10,24 +10,70 @@
   export let labelSrOnly = false;
 </script>
 
-<div class="grid grid-cols-3 w-full">
-  <label for={name} class="self-center" class:sr-only={labelSrOnly}>
+<div class="container" data-sr-only={labelSrOnly}>
+  <label for={name}>
     {label}
     {#if required}
-      <span class="ordinal">*</span>
+      <span class="required">*</span>
     {/if}
   </label>
-  <input
-    id={name}
-    {...$$restProps}
-    class={`${$$props.class ? `${$$props.class} ` : ''}input`}
-    class:col-span-2={!labelSrOnly}
-    class:col-span-3={labelSrOnly}
-    bind:value />
+  <input id={name} {...$$restProps} bind:value />
   {#if error}
-    <span
-      class="text-red-500 mt-1 text-xs font-semibold col-start-2 col-span-2">
+    <span class="error">
       {error}
     </span>
   {/if}
 </div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+
+  label {
+    align-self: center;
+  }
+
+  .container[data-sr-only='true'] > label {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+
+  .required {
+    font-variant-numeric: ordinal;
+  }
+
+  input {
+    padding: 0.25rem 0.75rem;
+    background-color: hsla(var(--gray-100));
+    border: 1px solid hsla(var(--gray-400));
+    border-radius: 0.125rem;
+    color: hsla(var(--gray-900));
+  }
+
+  .container[data-sr-only='true'] > input {
+    column-span: 3;
+  }
+  .container[data-sr-only='false'] > input {
+    column-span: 2;
+  }
+
+  .error {
+    margin-top: 0.25rem;
+    grid-column-start: 2;
+    column-span: 2;
+
+    font-size: 0.75rem;
+    line-height: 1rem;
+    font-weight: 600;
+    color: hsl(0, 72%, 50%);
+  }
+</style>
