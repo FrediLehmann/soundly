b789a28c014a1c62719c5090a19d992a597c939d
FrediLehmann <frederic.lehmann@bluewin.ch>
2021-12-24 16:04:07 +0000

    create search field

---
+++ src/lib/components/molecules/SearchField/SearchField.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+  import { userEvent, within } from '@storybook/testing-library';
+
+  import SearchField from './SearchField.svelte';
+</script>
+
+<Meta title="Molecules/SearchField" />
+
+<Story
+  name="SearchField"
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+    userEvent.hover(button);
+
+    const input = canvas.getByRole('textbox');
+    userEvent.hover(input);
+
+    userEvent.type(input, 'Search content');
+  }}>
+  <SearchField />
+</Story>
