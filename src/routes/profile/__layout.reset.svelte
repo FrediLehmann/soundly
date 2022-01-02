2c35ff3dda300b3eb967ab482ad001ab0f45e8ea
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-01-02 13:08:01 +0000

    profile binding

--- src/routes/profile/__layout.reset.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts" context="module">
-  import { PageFrame } from '$lib/components/atoms';
-</script>
-
-<PageFrame>
-  <slot />
-</PageFrame>
--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,13 +1,39 @@
 <script lang="ts" context="module">
   import { ArrowLeft } from '$lib/Icons';
   import { Link } from '$lib/components/atoms';
+  import { onMount } from 'svelte';
+  import { supabase } from '$lib/supabase';
+  import { goto } from '$app/navigation';
 </script>
 
-<Link href="/" class="mb-2 pt-4">
-  <ArrowLeft width="1.25rem" height="1.25rem" />
-  Home
-</Link>
+<script lang="ts">
+  let user: { name: string; email: string } = {
+    name: '',
+    email: ''
+  };
+
+  onMount(async () => {
+    let { data, error } = await supabase.from('profiles').select();
+    if (error) goto('/?type=error&message=Something went wrong.');
+
+    // If there is no profile yet we'll create one
+    if (data.length === 0) {
+      const res = await supabase
+        .from('profiles')
+        .insert({ user_id: supabase.auth.user().id });
+      if (res.error) goto('/?type=error&message=Something went wrong.');
+
+      data = res.data;
+    }
+
+    user.name = data[0].user_name || '';
+    user.email = supabase.auth.user().email;
+  });
+</script>
+
 <section class="grid-cols-2">
   <label class="col-span-1" for="email">Email</label>
-  <input class="col-span-1" id="email" />
+  <input class="col-span-1" id="email" value={user.email} />
+  <label class="col-span-1" for="username">Username</label>
+  <input class="col-span-1" id="username" value={user.name} />
 </section>
